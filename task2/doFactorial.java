155c5f660a3076fc81d71bc47f639f78c04bb6c4
liumce1@163.com <you@example.com>
2019-08-11 12:14:34 +0800

    实现斐波那契数列求值

--- task2/doFactorial.java
+++ task2/doFactorial.java
@@ -11,20 +11,20 @@ public class doFactorial {
         int result = 1;
         System.out.println("输入一个数字");
         Scanner scan = new Scanner(System.in);
-        if(scan.hasNext())
-        {
+        if (scan.hasNext()) {
             String str1 = scan.next();
             try {
                 num = Integer.parseInt(str1);
             } catch (NumberFormatException e) {
                 System.out.println("你输入的不是数字!");
                 e.printStackTrace();
+
+                for (int j = 1; j <= num; j++) {
+                    result = result * j;
+                }
+                System.out.println(num + "的阶乘为" + result);
             }
-            for (int j = 1; j <= num; j++) {
-                result = result * j;
-            }
-            System.out.println(num + "的阶乘为" + result);
+            scan.close();
         }
-        scan.close();
     }
 }
